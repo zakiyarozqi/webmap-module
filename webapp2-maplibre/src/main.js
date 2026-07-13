bf5d22b2b750acef4c4b967b70cd563df982a908
Zakiya Rozqi Auliya <zkyrozqi@gmail.com>
2026-07-13 18:39:32 +0700

    add raster data into webmap

--- webapp2-maplibre/src/main.js
+++ webapp2-maplibre/src/main.js
@@ -34,6 +34,8 @@ const map = new Map({
 // }
 
 map.on('load', () => {
+
+  //layer vektor titik
   map.addSource('kota', {
     type: "geojson",
     data: naturalEarthData
@@ -50,6 +52,8 @@ map.on('load', () => {
       "circle-stroke-color" : "black"
     }
   })
+
+  //layer vektor polygon
   map.addSource('pulau', {
     type: "geojson",
     data: areaData
@@ -65,7 +69,24 @@ map.on('load', () => {
     }
   })
 
+//layer raster
+map.addSource("spongebob", {
+    type: "image",
+    url: "https://static.wikia.nocookie.net/cartoons/images/e/ed/Profile_-_SpongeBob_SquarePants.png",
+    coordinates: [
+      [79.16, -0.40], // top-left
+      [94.18, -1.66], // top-right
+      [94.65, -14.73], // bottom-right
+      [72.97, -13.74] // bottom-left
+    ]
+  }) 
 
+  map.addLayer({
+    id: "spongebob-image",
+    type: "raster",
+    source: "spongebob",
+  })
 
+  
 })
 
